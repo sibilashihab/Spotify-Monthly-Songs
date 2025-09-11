da8c06cabbf554adc4ae1ff367fb451c49b6874e
Sibila <sibilashihab123@gmail.com>
2025-09-11 23:26:56 +0100

    logic done need to polish

--- playlist.py
+++ playlist.py
@@ -26,6 +26,10 @@ class save:
                                 headers = {"Content-Type": "application/json", 
                                            "Authorization": "Bearer {}".format(self.spotify_token)})
         
+        if response.status_code != 200:
+            print("Failed to check liked songs", response.json())
+            return
+        
         data = response.json()["items"]
         tracks =[]
         for i in data:
@@ -38,12 +42,16 @@ class save:
 
         return tracks
     
-    def createPlaylist(self, tracks):
+    def createPlaylist(self):
         #Check if playlist exists already if not add songs
         query = "https://api.spotify.com/v1/users/{}/playlists".format(self.spotify_user_id)
 
         response = requests.get(query, 
                                 headers = {"Authorization": "Bearer {}".format(self.spotify_token)})
+        
+        if response.status_code != 200:
+            print("Failed to check user playlists", response.json())
+            return
         
         playlists = response.json()["items"]
         
@@ -52,26 +60,79 @@ class save:
         for i in playlists:
             if i["name"] == current_month:
                 print("Playlist Already exists")
-                return     
+                return i["id"], current_month  
             
         newurl = "https://api.spotify.com/v1/users/{}/playlists".format(self.spotify_user_id)
 
         payload = {
         "name": current_month,
         "description": "{} monthly playlist".format(current_month),
-        "public": False
+        "public": False,
+        "collaborative": False ############ playlist is being publicly created for some reason
         }    
 
         create_response = requests.post(newurl, headers={
         "Authorization": "Bearer {}".format(self.spotify_token),
         "Content-Type": "application/json"
         }, json=payload)
-
+        
         if create_response.status_code == 201:
-            print(f"Playlist created for {current_month}")
+            print("Playlist created for {}".format(current_month))
+            playlist_id = create_response.json()["id"] #getting playlist id to add songs into it          
+            return playlist_id, current_month
         else:
             print("Failed to create playlist:", create_response.json())
+            return None
+    
+    def addSongs(self, newplaylistid, likedsongs, currentmonth):
+        #adding songs to the created playlist
+        #get playlist first
+
+        newsongs = "https://api.spotify.com/v1/playlists/{}/tracks".format(newplaylistid)
+
+        response = requests.get(newsongs, 
+                                headers = {"Authorization": "Bearer {}".format(self.spotify_token)})
+        
+        if response.status_code != 200:
+            print("Failed to get playlist", response.json())
+            return
+        
+        songs = response.json()["items"]
+        existingsongs=[]
+        for i in songs:
+            existingsongs.append(i["track"]["uri"]) #getting songs already in the playlist to avoid duplicates
+        
+        if response.status_code != 200:
+            print("Failed to get playlist", response.json())
+            return
+        
+        uris = [] #list to store all songs of the month
+        for song in likedsongs:
+            if song["date"] == currentmonth and song["uri"] not in existingsongs:
+                uris.append(song["uri"]) #getting uris from dictionary of liked tracks if it was added this month
+
+        if not uris:
+            print("No songs to add for this month.")
+            return
+
+        payload = {"uris": uris}    
+
+        create_response = requests.post(newsongs, headers={
+                "Authorization": "Bearer {}".format(self.spotify_token),
+                "Content-Type": "application/json"
+                }, json=payload)
+
+        if create_response.status_code != 201:
+            print("Failed to add tracks to playlist", create_response.json())
+            return
+        
+        else:
+            print("Tracks added!")
 
 a = save()
 likedsongs = a.findSongs()
-a.createPlaylist(likedsongs)
+newplaylistid, currentmonth = a.createPlaylist()
+if newplaylistid:
+    a.addSongs(newplaylistid,likedsongs, currentmonth)
+else:
+    print("Cannot create or get playlist. Skipping song addition.")
